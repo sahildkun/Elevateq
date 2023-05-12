eb5f4b8b6893c3731615395cf1cbf3419a428107
sahildkun <sahilsahoo23@gmail.com>
2023-05-12 17:55:18 +0530

    env not hiding

--- lib/auth.ts
+++ lib/auth.ts
@@ -9,7 +9,7 @@ const adapter = PrismaAdapter(prisma);
 export const authOptions: NextAuthOptions = {
   adapter: adapter, 
   secret: process.env.NEXTAUTH_SECRET,
-    providers: [
+    providers: [ 
       GoogleProvider({
         clientId: process.env.GOOGLE_CLIENT_ID!,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
